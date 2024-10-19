9b16c2cdcd674849009be9098e29c0605a1e0dd1
gouni <gounimahesh9000@gmail.com>
2024-10-19 18:55:01 +0530

    Initial commit

---
+++ src/main/java/com/example/jenkins/JenkinsCICd.java
@@ -0,0 +1,13 @@
+package com.example.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsCICd {
+    @GetMapping(value = "/dockerDemo")
+    public String helloWorld(){
+        System.out.println("hello");
+        return " CI demo now to demo ci";
+    }
+}
