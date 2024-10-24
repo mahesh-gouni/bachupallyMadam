d760dcba3cc883c8751e4755b4cace2eeb6ad6e8
gouni <gounimahesh9000@gmail.com>
2024-10-24 16:59:12 +0530

    updated dockerfiles job finally resolved webhook plug in

--- src/main/java/com/example/jenkins/WebhooksTest.java
+++ src/main/java/com/example/jenkins/WebhooksTest.java
@@ -3,5 +3,6 @@ package com.example.jenkins;
 public class WebhooksTest {
     public static void main(String[] args) {
         System.out.println("hello mahi");
+        System.out.println("plugin");
     }
 }
